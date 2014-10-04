f321cc38655ec838742bb43daa98dcfca9df9ccb
aditya01933 <adityatiwari01933@gmail.com>
2014-10-04 22:08:31 +0530

    before adding user info database

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20141004063710) do
     t.datetime "updated_at"
   end
 
+  
+
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
