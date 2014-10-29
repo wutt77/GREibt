be8de84039339dc65f7d6f1da14ff0aee5cac846
aditya01933 <adityatiwari01933@gmail.com>
2014-10-29 22:22:53 +0530

    user details already exist message added

--- app/models/userdetail.rb
+++ app/models/userdetail.rb
@@ -1,5 +1,5 @@
 class Userdetail < ActiveRecord::Base
 	belongs_to :user
   #validates that one user can create just one user details
-  validates_uniqueness_of :user_id
+  validates_uniqueness_of :user_id, :message => "details already exist"
 end
