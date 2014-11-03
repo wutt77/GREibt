d2509709614052cdbf21ca8def129c648ad5860a
aditya01933 <adityatiwari01933@gmail.com>
2014-11-03 12:28:50 +0530

    has many relation added

--- app/models/userdetail.rb
+++ app/models/userdetail.rb
@@ -2,4 +2,6 @@ class Userdetail < ActiveRecord::Base
 	belongs_to :user
   #validates that one user can create just one user details
   validates_uniqueness_of :user_id, :message => "details already exist"
+
+  has_many :posts
 end
